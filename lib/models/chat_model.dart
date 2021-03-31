2bb2b8e5794bb5eae4dbae8f350354e7468df640
Sunit Roy <2703iamsry@gmail.com>
2021-04-01 02:44:26 +0530

    Initial commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,119 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Sunit Roy",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Harvey Spectre",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Mike Ross",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Rachel",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+          "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Sunit Roy",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Harvey Spectre",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Mike Ross",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Rachel",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Sunit Roy",
+      message: "Hey Flutter, You are so amazing !",
+      time: "15:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Harvey Spectre",
+      message: "Hey I have hacked whatsapp!",
+      time: "17:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Mike Ross",
+      message: "Wassup !",
+      time: "5:00",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Rachel",
+      message: "I'm good!",
+      time: "10:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Barry Allen",
+      message: "I'm the fastest man alive!",
+      time: "12:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+  new ChatModel(
+      name: "Joe West",
+      message: "Hey Flutter, You are so cool !",
+      time: "15:30",
+      avatarUrl:
+      "https://user-images.githubusercontent.com/67560900/113205182-9ef68e80-928b-11eb-950a-960bc889ba02.jpg"),
+];
